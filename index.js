b9fbef0060176a2dec9d3fdf67e8da5b58653946
brandon <brandon@behemoth.ca>
2015-12-05 12:15:46 -0800

    Implemented recommendations from @jdalton (c75c7e7a0a14e2b47aa3544cdc843e0a4975f43e)

--- index.js
+++ index.js
@@ -26,9 +26,9 @@
          * @returns {String} the generated uuid
          */
         extendWith.uuid = function () {
-            var d = new Date().getTime();
+            var d = _.now();
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = (d + Math.random() * 16) % 16 | 0;
+                var r = (d + _.random(16)) % 16 | 0;
                 d = Math.floor(d / 16);
                 return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
             });
